8829af9e77c1a6f79fc77b339dd950474b5becd7
prvmjsky <lukyanovalexey1@gmail.com>
2025-04-24 16:53:13 +0500

    setup Makefiles, dependencies and plugins
    lombok, checkstyle, sonarqube, jacoco, ben-manes.versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
     id("java")
+    checkstyle
+    jacoco
+    id("org.sonarqube") version "6.1.0.5360"
+    id("com.github.ben-manes.versions") version "0.52.0"
 }
 
 group = "hexlet.code"
@@ -10,10 +14,28 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.11.4"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    compileOnly("org.projectlombok:lombok:1.18.36")
+    annotationProcessor("org.projectlombok:lombok:1.18.36")
 }
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy("jacocoTestReport")
+}
+
+tasks.jacocoTestReport {
+    reports {
+        dependsOn(tasks.test)
+        xml.required.set(true)
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "prvmjsky_java-project-72")
+        property("sonar.organization", "prvmjsky")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
