135a1e37bb55da8b9ee5f7591c9d090b02ab89ac
prvmjsky <lukyanovalexey1@gmail.com>
2025-07-14 14:50:29 +0500

    handle exceptions in checks creation

--- app/src/main/java/hexlet/code/controller/UrlChecksController.java
+++ app/src/main/java/hexlet/code/controller/UrlChecksController.java
@@ -25,23 +25,30 @@ public final class UrlChecksController {
         var urlId = ctx.pathParamAsClass("id", Long.class).get();
         var url = UrlRepository.find(urlId).orElseThrow(NotFoundResponse::new);
 
-        HttpResponse<String> response = Unirest.get(url.getName()).asString();
-        var status = response.getStatus();
+        try {
+            HttpResponse<String> response = Unirest.get(url.getName()).asString();
+            var status = response.getStatus();
 
-        Document body = Jsoup.parse(response.getBody());
-        var title = body.title();
+            Document body = Jsoup.parse(response.getBody());
+            var title = body.title();
 
-        Element h1Element = body.selectFirst("h1");
-        var h1 = h1Element != null ? h1Element.text() : null;
+            Element h1Element = body.selectFirst("h1");
+            var h1 = h1Element != null ? h1Element.text() : null;
 
-        Element descriptionElement = body.selectFirst("meta[name=description]");
-        var description = descriptionElement != null ? descriptionElement.attr("content") : null;
+            Element descriptionElement = body.selectFirst("meta[name=description]");
+            var description = descriptionElement != null ? descriptionElement.attr("content") : null;
 
-        var check = new UrlCheck(status, title, h1, description, urlId);
-        UrlCheckRepository.save(check);
+            var check = new UrlCheck(status, title, h1, description, urlId);
+            UrlCheckRepository.save(check);
+            ctx.sessionAttribute("flash", "Страница успешно проверена");
+            ctx.sessionAttribute("flash-type", "alert alert-success");
 
-        ctx.sessionAttribute("flash", "Страница успешно проверена");
-        ctx.sessionAttribute("flash-type", "alert alert-success");
-        ctx.redirect(NamedRoutes.urlPath(urlId));
+        } catch (Exception e) {
+            ctx.sessionAttribute("flash", "Не удалось проверить страницу");
+            ctx.sessionAttribute("flash-type", "alert alert-danger");
+
+        } finally {
+            ctx.redirect(NamedRoutes.urlPath(urlId));
+        }
     }
 }
